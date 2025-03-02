ce62758256188fca1122118d85cbe0f5c3ac03f5
Tom <tomburdge3@gmail.com>
2025-03-02 15:34:11 +0000

    feat: add into arrow for Games

--- src/games.rs
+++ src/games.rs
@@ -1,7 +1,13 @@
+use arrow::array::RecordBatch;
+use arrow_json::reader::ReaderBuilder;
+use arrow_schema::{DataType, Field, Schema};
 use dateparser::DateTimeUtc;
+use pyo3::exceptions::PyException;
 use pyo3::exceptions::PyValueError;
+use pyo3::PyErr;
 use pyo3::PyResult;
 use serde::{Deserialize, Serialize};
+use std::sync::Arc;
 
 use super::archives::PlayersArchives;
 use super::client::PyClient;
@@ -16,14 +22,65 @@ pub fn month_string_to_date(string: &str) -> PyResult<DateTimeUtc> {
     Ok(date)
 }
 
-#[derive(Deserialize, Serialize, Debug)]
 pub struct Games {
     games: Vec<Game>,
+    schema: Schema,
+}
+
+impl TryFrom<Games> for RecordBatch {
+    type Error = PyErr;
+
+    fn try_from(other: Games) -> Result<Self, Self::Error> {
+        let mut decoder = ReaderBuilder::new(Arc::new(other.schema)).build_decoder().map_err(|error|PyException::new_err(format!("Error with formatting when conv when converting schema input to arrow schema: {}", error)))?;
+        decoder.serialize(&other.games).map_err(|error|PyException::new_err(format!("Error with serializing the payloads when conv when converting schema input to arrow schema: {}", error)))?;
+        let batch = decoder
+            .flush()
+            .map_err(|error| {
+                PyException::new_err(format!("Error when flushing pyarrow batch: {}", error))
+            })?
+            .ok_or(PyValueError::new_err("Resulting pyarrow object was empty"))?;
+        Ok(batch)
+    }
 }
 
 impl Games {
-    pub fn new(_client: &PyClient, _archives: PlayersArchives) -> PyResult<Self> {
-        todo!("Add Games creator/getter process")
+    pub fn new(client: &PyClient, archives: PlayersArchives) -> PyResult<Self> {
+        let schema = Schema::new(vec![
+            Field::new("avatar", DataType::Utf8, false),
+            Field::new("player_id", DataType::Int32, false),
+            Field::new("@id", DataType::Utf8, false),
+            Field::new("url", DataType::Utf8, false),
+            Field::new("name", DataType::Utf8, false),
+            Field::new("username", DataType::Utf8, false),
+            Field::new("title", DataType::Utf8, true),
+            Field::new("followers", DataType::Int32, false),
+            Field::new("country", DataType::Utf8, false),
+            Field::new("location", DataType::Utf8, false),
+            Field::new("last_online", DataType::Int64, false),
+            Field::new("joined", DataType::Int64, false),
+            Field::new("status", DataType::Utf8, false),
+            Field::new("is_streamer", DataType::Boolean, false),
+            Field::new("verified", DataType::Boolean, false),
+            Field::new("league", DataType::Utf8, false),
+        ]);
+        let mut games = Games {
+            games: vec![],
+            schema,
+        };
+
+        for player_archive in archives.players {
+            for archive_url in player_archive.archives {
+                let res = client.get_url(&archive_url)?;
+                let game = serde_json::from_str::<Game>(&res).map_err(|error| {
+                    PyValueError::new_err(format!(
+                        "Error in parsing the payload into a game. {}",
+                        error
+                    ))
+                })?;
+                games.games.push(game);
+            }
+        }
+        Ok(games)
     }
 }
 
